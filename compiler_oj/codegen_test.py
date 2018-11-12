052bbed338bd66f867221832ebed94d2dce7683e
Engineev <ryw_pub@126.com>
2018-11-12 14:01:29 +0800

    update

--- compiler_oj/codegen_test.py
+++ compiler_oj/codegen_test.py
@@ -3,13 +3,13 @@ from . import assembly
 
 
 def test(testcase, bash_path):
-    res = subprocess.run(["bash", bash_path], input=testcase.src,
+    res = subprocess.run(["bash", bash_path], input=testcase.src.encode('utf-8'),
                          stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
     with open("./__a.asm", "w") as f:
-        f.write(res.stdout)
+        f.write(res.stdout.decode('utf-8'))
     
     try:
-        flag, res = assembly.run("__a.asm", input=testcase.input,
+        flag, res = assembly.run("__a.asm", input=testcase.input.encode('utf-8'),
                                  timeout=testcase.timeout)
     except subprocess.TimeoutExpired:
         return False, "TLE"
@@ -19,7 +19,8 @@ def test(testcase, bash_path):
     if testcase.assertion == "output":
         if res.returncode != 0:
             return False, "Unexpected runtime error"
-        output = '\n'.join([x.strip() for x in res.stdout.split('\n')])
+        output = '\n'.join([x.strip() 
+                            for x in res.stdout.decode('utf-8').split('\n')])
         if output.strip() == testcase.output.strip():
             return True, ""
         return False, "output dismatched"
